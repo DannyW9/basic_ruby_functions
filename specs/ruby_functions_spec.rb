11414b1c74f844b70a8fc7e96c04f16089368588
Danny Welsh <dannywelsh400@hotmail.com>
2019-03-19 16:44:42 +0000

    adds fifth function (divide)

--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -20,13 +20,11 @@ class FunctionsTest < MiniTest::Test
   end
 
   def test_multiply()
-    skip
     multiply_result = multiply( 4, 2 )
     assert_equal( 8, multiply_result )
   end
 
   def test_divide()
-    skip
     divide_result = divide( 10, 2 )
     assert_equal( 5, divide_result )
   end
